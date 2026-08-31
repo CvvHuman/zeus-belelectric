e6fbd7ab985be5d36fa52b07da159ff249803fb2
Vlad <92922200+CvvHuman@users.noreply.github.com>
2026-08-31 18:04:19 +0300

    Update layout Metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import YandexMetrika from '@/components/YandexMetrika'; 
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <YandexMetrika />
+      </body>
     </html>
   );
 }
