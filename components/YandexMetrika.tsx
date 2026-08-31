c844ec1e7b1527c5a2726735b9db18a395ae1986
Vlad <92922200+CvvHuman@users.noreply.github.com>
2026-08-31 18:02:29 +0300

    Update YandexMetrika

--- components/YandexMetrika.tsx
+++ components/YandexMetrika.tsx
@@ -1,1 +1,48 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
+
+export default function YandexMetrika() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && typeof (window as any).ym === 'function') {
+      const url = `${pathname}${searchParams.toString() ? `?${searchParams.toString()}` : ''}`;
+      (window as any).ym(112113946, 'hit', url);
+    }
+  }, [pathname, searchParams]);
+
+  return (
+    <>
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(112113946, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });
+        `}
+      </Script>
+      <noscript>
+        <div>
+          <img 
+            src="https://mc.yandex.ru/watch/112113946" 
+            style={{ position: 'absolute', left: '-9999px' }} 
+            alt="" 
+          />
+        </div>
+      </noscript>
+    </>
+  );
+}
 
