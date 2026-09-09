04d6730615a4382bbc7525fa776e5a6a59e17ada
Vlad <92922200+CvvHuman@users.noreply.github.com>
2026-09-09 20:58:14 +0300

    Update3.tsx

--- components/YandexMetrika.tsx
+++ components/YandexMetrika.tsx
@@ -1,30 +1,42 @@
 'use client';
 
-import Script from 'next/script';
-
 export default function YandexMetrika() {
   return (
     <>
-      <Script
+      {/* Прямое жесткое подключение тега скрипта без оптимизаций Next.js */}
+      <script
+        async
         src="https://yandex.ru"
-        strategy="afterInteractive"
       />
-      <Script id="yandex-metrika-init" strategy="afterInteractive">
-        {`
-          window.ym = window.ym || function() {
-            (window.ym.a = window.ym.a || []).push(arguments)
-          };
-          window.ym.l = 1 * new Date();
+      
+      {/* Прямая инициализация */}
+      <script
+        id="yandex-metrika-init"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.ym = window.ym || function() {
+              (window.ym.a = window.ym.a || []).push(arguments)
+            };
+            window.ym.l = 1 * new Date();
 
-          ym(112113946, "init", {
-               clickmap:true,
-               trackLinks:true,
-               accurateTrackBounce:true,
-               webvisor:true,
-               trackHash:true // Обязательно для одностраничников на Next.js
-          });
-        `}
-      </Script>
+            ym(112113946, "init", {
+                 clickmap:true,
+                 trackLinks:true,
+                 accurateTrackBounce:true,
+                 webvisor:true
+            });
+          `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img 
+            src="https://yandex.ru" 
+            style={{ position: 'absolute', left: '-9999px' }} 
+            alt="" 
+          />
+        </div>
+      </noscript>
     </>
   );
 }
